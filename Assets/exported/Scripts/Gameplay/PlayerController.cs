bae39d4690f309af7c26329b80fa870b78098a69
class <geri271098@gmail.com>
2019-01-05 22:06:56 +0100

    #REPOST first lvl

---
+++ Assets/exported/Scripts/Gameplay/PlayerController.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    #region Variables
+    [Header("Movement Settings:")]
+    public float speed;
+    public float jumpForce;
+
+    [Header("Physics:")]
+    public float fallMultiplier = 2.5f;
+    public LayerMask discludePlayer;
+    public Transform raycastPosition;
+    private bool grounded;
+
+    //References
+    [Header("References:")]
+    public Transform yBotT;
+    public GameObject projectile1;
+    private Rigidbody rb;
+    private SphereCollider spColl;
+
+    [Header("References:")]
+    public Animator anim;
+
+    //Utilities
+    float horizontalInput, verticalInput;
+    Vector3 movementDirection, velocity;
+    bool playerMoving;
+    #endregion
+
+    #region Core
+    // Use this for initialization
+    void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+        spColl = gameObject.GetComponent<SphereCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GroundChecking();
+        Jump();
+        Move();
+        Combat();
+        Animate();
+    }
+    #endregion
+
+    #region Movement
+    private void Move()
+    {
+        //Get Inputs
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        
+        //Generate direction vector
+        movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+
+        //Is the player moving?
+        velocity = new Vector3(horizontalInput, 0, verticalInput);
+
+        if (velocity != Vector3.zero)
+            playerMoving = true;
+        else
+            playerMoving = false;
+
+        //Modify move vector
+        if (verticalInput > 0)
+            movementDirection = transform.forward;
+        else if (verticalInput < 0)
+            movementDirection = -transform.forward;      
+
+        if (horizontalInput > 0) 
+            movementDirection = transform.right;
+        else if (horizontalInput < 0)
+            movementDirection = -transform.right;
+
+        //Add force in direction with speed
+        rb.AddForce(movementDirection * speed * Time.deltaTime, ForceMode.Force);
+    }
+
+    private void Jump()
+    {
+        if (grounded && Input.GetButton("Jump"))
+        {
+            anim.SetTrigger("Jump");
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+    #endregion
+
+    #region Animations
+    //Function charged of animating the player following certain inputs
+    private void Animate()
+    {
+        anim.SetFloat("Speed", velocity.z);
+        anim.SetFloat("Direction", velocity.x);
+        anim.SetBool("IsMoving", playerMoving);
+        anim.SetBool("IsGrounded", grounded);
+    }
+    #endregion
+
+    #region Utilities
+    private void FixedUpdate()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    private void GroundChecking()
+    {
+       
+        bool touches = Physics.Raycast(raycastPosition.transform.position, Vector3.down, 0.3f, discludePlayer);
+
+        if (touches)
+            grounded = true;
+        else
+            grounded = false;
+
+        Debug.DrawRay(raycastPosition.transform.position, Vector3.down * 0.3f, Color.red);
+    }
+    #endregion
+
+    #region Combat
+    private void Combat()
+    {
+        //Check inputs
+        if (Input.GetButton("Fire1"))
+        {
+            //Play animation
+            anim.SetTrigger("Spell1");
+            StartCoroutine(CastSpell());
+        }
+    }
+
+    IEnumerator CastSpell()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        //Cast projectile
+        Instantiate(projectile1, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1.0f), transform.rotation);
+    }
+    #endregion
+}
---
+++ Assets/exported/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    #region Variables
+    public Sound[] sounds; //Array of sounds
+    public static AudioManager instance;
+    #endregion
+
+    // Use this for initialization
+    private void Awake()
+    {
+
+        //Check that there's only one AudioManager in the scene
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this.gameObject);
+
+        //Make sure that the AudioManager doesn't get destroyed
+        DontDestroyOnLoad(this.gameObject);
+
+        //Creat an AudioSource for each sound
+        foreach (Sound sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.loop;
+        }
+    }
+
+    //Function that playes a sound based on the name given
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found !");
+            return;
+        }
+        s.source.Play();
+    }
+
+    //Function that playes a sound based on the name given with a random pitch
+    public void PlayWithRandomPitch(string name, float min, float max)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found !");
+            return;
+        }
+
+        s.source.pitch = UnityEngine.Random.Range(min, max);
+        s.source.Play();
+    }
+
+    public void Pause(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found !");
+            return;
+        }
+        s.source.Pause();
+    }
+}
