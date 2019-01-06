035b8bc5692a4a8660e6b8aea83cc1f2577e7781
Alex Espada <alex.espada@enti.cat>
2019-01-06 17:40:01 +0100

    Enemy AI

---
+++ Assets/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Elemento
+{
+    public class Enemy : MonoBehaviour
+    {
+
+        public float lookRadius = 1f;
+        Transform target;
+        NavMeshAgent agent;
+
+        private void Start()
+        {
+            target = GameObject.Find("Player").GetComponent<Transform>();
+            agent = GetComponent<NavMeshAgent>();
+        }
+        private void Update()
+        {
+            float dist = Vector3.Distance(target.position, transform.position);
+            
+            if (dist <= lookRadius)
+                agent.SetDestination(target.position);
+        }
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, lookRadius);
+        }
+
+    }
+}
